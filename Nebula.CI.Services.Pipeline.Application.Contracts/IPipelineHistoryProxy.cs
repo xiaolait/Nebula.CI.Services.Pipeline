053c319300b25c066a356ea9477aa6e78d8d3cb4
xiaolait <xiaolait@outlook.com>
2020-10-09 20:09:38 +0800

    Create PipelineHistory proxy

---
+++ Nebula.CI.Services.Pipeline.Application.Contracts/IPipelineHistoryProxy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Nebula.CI.Services.Pipeline
+{
+    public interface IPipelineHistoryProxy : ITransientDependency
+    {
+        Task CreateAsync(PipelineDto input);
+    }
+}
--- Nebula.CI.Services.Pipeline.Application/DomainEventHandlers/PipelineRunHandler.cs
+++ Nebula.CI.Services.Pipeline.Application/DomainEventHandlers/PipelineRunHandler.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.EventBus;
+using Volo.Abp.ObjectMapping;
 
 namespace Nebula.CI.Services.Pipeline
 {
     public class PipelineRunHandler : ILocalEventHandler<PipelineRunEvent>, ITransientDependency
     {
+        private readonly IObjectMapper<PipelineApplicationModule> _objectMapper;
+        private readonly IPipelineHistoryProxy _pipelineHistoryProxy;
+
+        public PipelineRunHandler(IObjectMapper<PipelineApplicationModule> objectMapper, IPipelineHistoryProxy pipelineHistoryProxy = null)
+        {
+            _objectMapper = objectMapper;
+            _pipelineHistoryProxy = pipelineHistoryProxy;
+        }
 
         public async Task HandleEventAsync(PipelineRunEvent eventData)
         {
-            await Task.Delay(100);
-            Console.WriteLine($"create new pipelinehistory:{eventData.Pipeline.Name}");
+            var pipelineDto = _objectMapper.Map<Pipeline, PipelineDto>(eventData.Pipeline);
+            await _pipelineHistoryProxy?.CreateAsync(pipelineDto);
+
+            Console.WriteLine("Create new PipelineHistory");
         }
     }
 }
