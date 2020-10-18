1e8ed39be264bfdfd5b294b163bcf20ffaf54160
xiaolait <xiaolait@outlook.com>
2020-10-18 15:53:30 +0800

    Add Delete Event

--- Nebula.CI.Services.Pipeline.Application/DomainEventHandlers/PipelineDeletedHandler.cs
+++ Nebula.CI.Services.Pipeline.Application/DomainEventHandlers/PipelineDeletedHandler.cs
@@ -3,14 +3,23 @@ using System.Threading.Tasks;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Entities.Events;
 using Volo.Abp.EventBus;
+using Volo.Abp.EventBus.Distributed;
 
 namespace Nebula.CI.Services.Pipeline
 {
     public class PipelineDeletedHandler : ILocalEventHandler<EntityDeletedEventData<Pipeline>>, ITransientDependency
     {
+        private readonly IDistributedEventBus _distributedEventBus;
+
+        public PipelineDeletedHandler(IDistributedEventBus distributedEventBus)
+        {
+            _distributedEventBus = distributedEventBus;
+        }
+
         public async Task HandleEventAsync(EntityDeletedEventData<Pipeline> eventData)
         {
             Console.WriteLine($"pipeline:{eventData.Entity.Id} is deleted");
+            await _distributedEventBus.PublishAsync(new PipelineDeletedEto { Id = eventData.Entity.Id });
         }
     }
 }
---
+++ Nebula.CI.Services.Pipeline.Application/Etos/PipelineDeletedEto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Volo.Abp.EventBus;
+
+namespace Nebula.CI.Services.Pipeline
+{
+    [EventName("PipelineDeleted")]
+    public class PipelineDeletedEto
+    {
+        public int Id { get; set; }
+    }
+}
